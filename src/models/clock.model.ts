7799c14f3bdfcba728fc9562455ab0703d4fcd09
max <maxcalderon@campus.technion.ac.il>
2022-03-31 22:56:08 +0300

    made canvases in html. todo: need to move text components to the other canvas

--- src/models/clock.model.ts
+++ src/models/clock.model.ts
@@ -12,12 +12,6 @@ export class Clock extends AbstractGameComponent{
         super(color, x, y, myGameArea);
         this.fontSize = fontSize;
         this.fontStyle = fontStyle;
-        this.canvas = document.createElement("canvas"),
-        this.canvas.width = environment.canvasWidth;
-        this.canvas.height = environment.canvasHeight;
-        this.canvas.style.marginLeft = "800px";
-        this.canvas.style.marginTop = "-450px";
-        document.body.insertBefore(this.canvas, document.body.childNodes[1]);
     }
 
     updateTime() {
@@ -34,10 +28,10 @@ export class Clock extends AbstractGameComponent{
     }
 
     update() {
-        let ctx = this.canvas.getContext("2d");
-        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        ctx.font = this.fontSize + " " + this.fontStyle;
-        ctx.fillStyle = this.color;
-        ctx.fillText('Time left to speed up: ' + (environment.timeToSpeedUp - Clock.time), this.x, this.y);
+        // let ctx = this.canvas.getContext("2d");
+        // ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        // ctx.font = this.fontSize + " " + this.fontStyle;
+        // ctx.fillStyle = this.color;
+        // ctx.fillText('Time left to speed up: ' + (environment.timeToSpeedUp - Clock.time), this.x, this.y);
     }
 }
